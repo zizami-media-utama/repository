79f5332a4b494558fdb624fec818687851c38275
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-12 02:12:37 +0700

    update

--- slide/assets/slider.js
+++ slide/assets/slider.js
@@ -1,19 +1,29 @@
+/**
+ * Impress js slideshow for yuros.org
+ * Copyright@2025 Al Muhdil karim
+ * PT Lektor Media Utama
+ */
 
 
+// remove all location hash
+window.location.hash = '';
+
+// start slide js
 impress().init();
 
 
-let next_actio = document.getElementById('next-slide');
-let prev_actio = document.getElementById('prev-slide');
 let page_audio = ""
 
 
 // check current slide value and replace as audio nama value 
 document.addEventListener("impress:stepenter", function(event) {
-    let slide = event.target;
-    let posts = slide.id;
-    let value = posts.replace('step-','slide-');
-    page_audio = value+".mp3";
+    let posts = event.target.id;
+
+    if (( posts !== "step-1" )) {
+        let value = posts.replace('step-','slide-');
+        page_audio = value+".mp3";
+        audio_load();
+    }
 });
 
 
@@ -32,14 +42,13 @@ let audio_load = () => {
 }
 
 
-prev_actio.addEventListener("click", ()=>{
+document.getElementById('prev-slide').addEventListener("click", (event)=>{
+    event.preventDefault();
     impress().prev();
-    audio_load();
 })
 
 
-next_actio.addEventListener("click", ()=>{
+document.getElementById('next-slide').addEventListener("click", (event)=>{
+    event.preventDefault();
     impress().next();
-    audio_load();
 })
-
