918ffc1cd9a90a55f932a4f2de8578564ba3c75f
almuhdilkarim <almuhdilkarim2@mail.com>
2025-06-12 08:22:13 +0700

    update

--- slide/assets/accent.js
+++ slide/assets/accent.js
@@ -1,5 +1,5 @@
 function particle(){
-    particlesJS('particles-js',
+    particlesJS('wall',
 		{
 			"particles": {
 			"number": {
--- slide/assets/slider.js
+++ slide/assets/slider.js
@@ -5,50 +5,164 @@
  */
 
 
-// remove all location hash
-window.location.hash = '';
+// Global Variable
+let page_audio = document.getElementById('main-player');
+let page_timer = document.getElementById('progressbar');
+let page_progs = document.getElementById('progrestime');
 
-// start slide js
+
+// PREPARATION
+window.location.hash = ''; // clean url hash, because of url hash cause error for audio 
 impress().init();
 
 
-let page_audio = ""
+
+// AUDIO LIBRARY
+let audio_init = (posts)=> {
+
+    page_timer.style.width = "0%";
+
+    let value = posts.replace('step-','slide-');
+    let named = value+".mp3";
+    let aruri = window.location.pathname.split('/');
+    let rpath = aruri[aruri.length - 1];
+    let vpath = rpath.replace('.html','/');
+    let files = vpath+named;
+    page_audio.src = files;
+    page_audio.load();
+
+    page_audio.addEventListener('canplay', () => {
+        page_audio.play();
+        audio_prog();
+    });
+}
 
 
-// check current slide value and replace as audio nama value 
+let audio_exit = ()=> {
+    page_progs.classList.remove('active');
+    page_audio.pause();
+    page_audio.currentTime = 0;
+    page_audio.src = ""
+}
+
+
+let audio_mute = ()=> {
+    page_audio.muted = ! page_audio.muted;
+}
+
+
+let audio_play = ()=> {
+    ( page_audio.paused ) ? page_audio.play() : page_audio.pause();
+}
+
+
+let audio_prog = ()=> {
+
+    let plays = document.getElementById('pend-icon');
+    let mutes = document.getElementById('mute-icon');
+
+   
+    page_audio.addEventListener('timeupdate', () => {
+        const progress = (page_audio.currentTime / page_audio.duration) * 100;
+        page_timer.style.width = progress + '%';
+    });
+   
+    
+    page_audio.addEventListener("pause", (event) => { 
+        let icon = plays.getAttribute('data-play');
+        plays.src = icon
+        page_progs.classList.remove('active');
+    })
+
+    page_audio.addEventListener("play", (event) => { 
+        let icon = plays.getAttribute('data-pause');
+        plays.src = icon
+        page_progs.classList.add('active');
+    })
+
+    page_audio.addEventListener("mute", (event) => { 
+        let icon = mutes.getAttribute('data-unmute');
+        mutes.src = icon
+    })
+
+    page_audio.addEventListener("unmute", (event) => { 
+        let icon = mutes.getAttribute('data-muted');
+        mutes.src = icon
+    })
+}
+
+
+// CONTROL LIBRARY
+
+let control_init = ()=> {
+    document.getElementById('panel').classList.remove('slide-on-stop');
+    document.getElementById('panel').classList.add('slide-on-play');
+}
+
+
+let control_exit = ()=> {
+    document.getElementById('panel').classList.remove('slide-on-play');
+    document.getElementById('panel').classList.add('slide-on-stop');
+}
+
+
+let control_next = ()=> {
+    audio_exit();
+    impress().next();
+}
+
+
+let control_prev = ()=> {
+    audio_exit();
+    impress().prev()
+}
+
+
+// EVENT LISTENER 
 document.addEventListener("impress:stepenter", function(event) {
+    
     let posts = event.target.id;
 
-    if (( posts !== "step-1" )) {
-        let value = posts.replace('step-','slide-');
-        page_audio = value+".mp3";
-        audio_load();
+    if ( posts !== "step-1" ) {
+        control_init();
+        audio_init(posts);
+    }
+    else {
+        control_exit();
+        audio_exit();
     }
 });
 
 
-// get current slide audio name and play audio
-let audio_load = () => {
 
-    let aruri = window.location.pathname.split('/');
-    let rpath = aruri[aruri.length - 1];
-    let vpath = rpath.replace('.html','/');
-    let files = vpath+page_audio;
-    let plays = document.getElementById('main-player');
+// PLAYER CONTROL
 
-    plays.src = files;
-    plays.load();
-    plays.play();
-}
-
-
-document.getElementById('prev-slide').addEventListener("click", (event)=>{
-    event.preventDefault();
-    impress().prev();
+// first play
+document.getElementById('play-slide').addEventListener('click', (event)=> {
+    control_init();
+    impress().next();
 })
 
 
+// mute audio
+document.getElementById('mute-audio').addEventListener("click", (event)=>{
+    event.preventDefault(); audio_mute();
+})
+
+
+// play audio
+document.getElementById('pend-audio').addEventListener("click", (event)=>{
+    event.preventDefault(); audio_play();
+})
+
+
+// next slide
 document.getElementById('next-slide').addEventListener("click", (event)=>{
-    event.preventDefault();
-    impress().next();
+    event.preventDefault(); control_next();
+})
+
+
+// prev slide
+document.getElementById('prev-slide').addEventListener("click", (event)=>{
+    event.preventDefault(); control_prev();
 })
